a93f43b57e2c48b26725559125c51fe43359958d
pawelek3113 <pawel.kamil.komendera@gmail.com>
2023-08-13 13:01:26 +0200

    Resolve PR: avoid line repeating, update choices, delete Member import, make ban guild only

--- src/ext/moderation.py
+++ src/ext/moderation.py
@@ -1,6 +1,8 @@
 import typing as t
+from datetime import timedelta
 
-from nextcord import Forbidden, HTTPException, Interaction, Member, Permissions, SlashOption, User, slash_command
+from nextcord import Forbidden, HTTPException, Interaction, Permissions, SlashOption, User, slash_command
+from nextcord.ext import application_checks
 
 from . import BaseCog
 
@@ -13,25 +15,25 @@ class Moderation(BaseCog):
         super().__init__(bot)
 
     @slash_command(description="Ban user", default_member_permissions=Permissions(administrator=True))
+    @application_checks.guild_only()
     async def ban(
         self,
         interaction: Interaction,
         user: User,
         reason: str = SlashOption(description="Banning reason", required=False, default="No reason given"),
-        clear_messages: str = SlashOption(
+        clear_messages: bool = SlashOption(
             description="Clear user's messages",
-            choices=["Yes", "No"],
-            default="No",
+            choices={"Yes": True, "No": False},
+            default=False,
             required=False,
         ),
     ):
         try:
-            if clear_messages == "No":
+            if clear_messages == False:
                 await interaction.guild.ban(user=user, reason=reason, delete_message_seconds=0)
-                await interaction.send(f"{interaction.user.mention} banned {user.display_name}! Reason: {reason}")
             else:
                 await interaction.guild.ban(user=user, reason=reason, delete_message_seconds=604800)
-                await interaction.send(f"{interaction.user.mention} banned {user.display_name}! Reason: {reason}")
+            await interaction.send(f"{interaction.user.mention} banned {user.display_name}! Reason: {reason}")
         except Forbidden:
             await interaction.send(f"{interaction.user.mention}, you've got no permission to perform this command!")
         except HTTPException:
