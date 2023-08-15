3e5226808ce05496e7f753cbe7b24b424e465076
pawelek3113 <pawel.kamil.komendera@gmail.com>
2023-08-15 13:52:54 +0200

    Add kick moderation command

--- src/ext/moderation.py
+++ src/ext/moderation.py
@@ -19,7 +19,7 @@ class Moderation(BaseCog):
         self,
         interaction: Interaction,
         user: User,
-        reason: str = SlashOption(description="Banning reason", required=False, default="No reason given"),
+        reason: str = SlashOption(description="Ban reason", required=False, default="No reason given"),
         clear_messages: bool = SlashOption(
             description="Clear user's messages",
             choices={"Yes": True, "No": False},
@@ -45,3 +45,27 @@ class Moderation(BaseCog):
             )
         except HTTPException:
             await interaction.send(f"{interaction.user.mention}, banning failed.", ephemeral=True)
+
+    @slash_command(description="Kick user", default_member_permissions=Permissions(kick_members=True))
+    @application_checks.guild_only()
+    async def kick(
+        self,
+        interaction: Interaction,
+        user: User,
+        reason: str = SlashOption(description="Kick reason", required=False, default="No reason given"),
+    ):
+        try:
+            if user is None:
+                await interaction.send("Something went wrong")
+                return
+            if interaction.user == user:
+                await interaction.send(f"{interaction.user.mention}, you can't kick yourself!", ephemeral=True)
+                return
+            await interaction.guild.kick(user=user, reason=reason)
+            await interaction.send(f"{interaction.user.mention} kicked {user.mention}! Reason: {reason}")
+        except Forbidden:
+            await interaction.send(
+                f"{interaction.user.mention}, you've got no permission to perform this command!", ephemeral=True
+            )
+        except HTTPException:
+            await interaction.send(f"{interaction.user.mention}, unbanning failed.", ephemeral=True)
